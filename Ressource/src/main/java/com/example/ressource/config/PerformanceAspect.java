63f330997ad1ab57210479f47cfb3e26435bd69c
YosrBenAmor <100871594+YosrBenAmor@users.noreply.github.com>
2025-04-13 13:44:01 +0100

    Update PerformanceAspect.java

--- Ressource/src/main/java/com/example/ressource/config/PerformanceAspect.java
+++ Ressource/src/main/java/com/example/ressource/config/PerformanceAspect.java
@@ -1,8 +1,6 @@
  package com.example.ressource.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
